3b300bae0232517920b7a83652e15f2706502cd9
Gaston <Gaston@DESKTOP-JSDTFT8>
2020-06-01 14:48:08 -0300

    Listos, los puntos 2g,2h,2i y 2j

--- Grupo06/src/misAplicaciones/AppTPO.java
+++ Grupo06/src/misAplicaciones/AppTPO.java
@@ -40,9 +40,12 @@ public class AppTPO {
 
 		contidadMateriasOptativas(meteriaList);		
 		materiasComunesConDiccionario(meteriaList);
+		
+		
 
 	}
 
+	
 	public static class Materias 
 	{
 		public Integer nroCarrera;
@@ -313,9 +316,293 @@ public class AppTPO {
 	}
 
 
+	/**
+	@Tarea :   2G. Carrera que comparte mas del 80% de las materias de otra carrera
+	@Parámetros
+	@Devuelve
+	@Precondición
+	@Postcondición
+	@Costo
+	 **/
+	public static void compararCarreras(DiccionarioMultipleTDA dic) {
+		int c = contadorConjunto(dic.Claves());
+		int i, j;
+		boolean cumple;
+		int[] vectorCarreras = carrerasAVector(dic.Claves());
+		for(i=0; i<c; i++) {
+			for(j=1; j<c; j++) {
+				if(j>i) {
+					cumple = cumplePorcentaje(dic.Recuperar(vectorCarreras[i]), dic.Recuperar(vectorCarreras[j]));
+					if(cumple) {
+						System.out.println("La carrera" + vectorCarreras[i] + "comparte mas del 80% de las materias de la carrera " + vectorCarreras[j]);
+					}
+					cumple = cumplePorcentaje(dic.Recuperar(vectorCarreras[j]), dic.Recuperar(vectorCarreras[i]));
+					if(cumple) {
+						System.out.println("La carrera" + vectorCarreras[j] + "comparte mas del 80% de las materias de la carrera " + vectorCarreras[i]);
+					}
+				}
+			}
+		}
+	}
+	public static int contadorConjunto(ConjuntoTDA a) {
+		ConjuntoTDA aux = new ConjuntoLD();
+		aux.InicializarConjunto();
+		int c = 0;
+		while(!a.ConjuntoVacio()) {
+			c++;
+			aux.Agregar(a.Elegir());
+			a.Sacar(a.Elegir());
+		}
+		PasarConjunto(aux, a);
+		return c;
+	}
+	public static int[] carrerasAVector(ConjuntoTDA a) {
+		ConjuntoTDA aux = new ConjuntoLD();
+		aux.InicializarConjunto();
+		int c = contadorConjunto(a);
+		int[] carreras = new int[c];
+		c-=1;
+		while((!a.ConjuntoVacio()) && c>=0) {
+			carreras[c]=a.Elegir();
+			aux.Agregar(a.Elegir());
+			a.Sacar(a.Elegir());
+			c--;
+		}
+		PasarConjunto(aux,a);
+		return carreras;
+		
+	}
+	public static boolean cumplePorcentaje(ConjuntoTDA a, ConjuntoTDA b) {
+		int c = 0;
+		int cantMaterias = contadorConjunto(b);
+		boolean cumpleCondicion = false;
+		while(!a.ConjuntoVacio()) {
+			if (b.Pertenece(a.Elegir())) {
+				c++;
+			}
+			a.Sacar(a.Elegir());
+		}
+		int porcentaje = (c*100)/cantMaterias;
+		System.out.println(porcentaje);
+		if(porcentaje>80) {
+			cumpleCondicion = true;
+		}
+		return cumpleCondicion;
+	}
+	/**
+	@Tarea :   2H. Materias que no comparten ninguna carrera
+	@Parámetros
+	@Devuelve
+	@Precondición
+	@Postcondición
+	@Costo
+	 **/
+	public static void materiasUnicaCarrera(DiccionarioMultipleTDA dic) {
+		int cantCarreras = contadorConjunto(dic.Claves());
+		int[] codCarreras = new int[cantCarreras];
+		boolean pertenece;
+		codCarreras = carrerasAVector(dic.Claves());
+		int cantMaterias;
+		for(int i=0; i<cantCarreras; i++) {
+			cantMaterias = contadorConjunto(dic.Recuperar(codCarreras[i]));
+			int[] materias = new int[cantMaterias];
+			materias = carrerasAVector(dic.Recuperar(codCarreras[i]));
+			for (int j=0; j<cantMaterias; j++) {
+				pertenece = false;
+				for(int x=0; x<cantCarreras; x++) {
+					if (x!=i && pertenece==false) {
+						pertenece = dic.Recuperar(codCarreras[x]).Pertenece(materias[j]);
+					}
+				}
+				if(!pertenece) {
+					System.out.println("La materia " + materias[j] + " pertenece unicamente a la carrera "  + codCarreras[i]);
+				}
+			}
+		}
+	}
+	
 
+	/**
+	@Tarea 2 i
+	@Parámetros
+	@Devuelve
+	@Precondición
+	@Postcondición
+	@Costo
+	 **/
+	
+	public static void matrelacionadas20(DiccionarioMultipleTDA var){
+		int matcoinc = 0;
+		int mattotal = 0;
+		int contador = 0;
+		int c = 0;
+		ConjuntoTDA claves1 = var.Claves();
+		ConjuntoTDA claves2 = var.Claves();		
+		ConjuntoTDA clavesrep = new ConjuntoLD();
+		clavesrep.InicializarConjunto();
+		ConjuntoTDA matrel = new ConjuntoLD();
+		matrel.InicializarConjunto();
+		System.out.println("2-I Carrera/s que sólo tengan el 20% de las materias de otra carrera, indicando las carreras relacionadas.");
+		while(!claves1.ConjuntoVacio()) {
+			while(!claves2.ConjuntoVacio()){
+				ConjuntoTDA valores1 = var.Recuperar(claves1.Elegir());
+				ConjuntoTDA valores2 = var.Recuperar(claves2.Elegir());
+					if (!clavesrep.Pertenece(claves1.Elegir()) && !clavesrep.Pertenece(claves2.Elegir()) && !valores1.ConjuntoVacio() && claves1.Elegir() != claves2.Elegir()) {
+							while(!valores2.ConjuntoVacio()) {
+								mattotal ++;
+								if (valores1.Pertenece(valores2.Elegir())) 
+									matcoinc ++;
+								valores2.Sacar(valores2.Elegir());
+							}
+							contador ++;
+							c = porcentaje(mattotal, matcoinc, claves1.Elegir(), claves2.Elegir(), c);
+							mattotal = 0;
+							matcoinc = 0;
+							claves2.Sacar(claves2.Elegir());
+							if (!claves2.ConjuntoVacio())
+								valores2 = var.Recuperar(claves2.Elegir());
+							}
 
+					else {
+						claves2.Sacar(claves1.Elegir());
+				}
+			}
+			claves1.Sacar(claves1.Elegir());
+			claves2 = var.Claves();
+		}
+		if (contador == c)
+			System.out.println("No se encontraron carreras con el 20% de sus materias en común.");
+	}
+	
+	
+	
+	private static int porcentaje(int total, int tengo, int clave1, int clave2, int contador) {
+		int a = 0;
+		a = tengo * 100 / total;
+		if (a == 20)
+			System.out.println("La Carrera con código " + clave1 + " tiene en común con la carrera "+ clave2 + " el " + a + "% de sus materias.");
+		else {
+			contador ++;
+		}
+		return contador;
+	}
+	
+	
+	/**
+	@Tarea 2 j
+	@Parámetros
+	@Devuelve
+	@Precondición
+	@Postcondición
+	@Costo
+	 **/
+	
+	public static void matnocomunes(DiccionarioMultipleTDA var) {
+		ConjuntoTDA carreras1 = var.Claves();
+		ConjuntoTDA carreras2 = var.Claves();
+		ConjuntoTDA repasadas = new ConjuntoLD();
+		System.out.println("2-J Para cada combinación de dos carreras, indicar materias no comunes. ");
+		while (!carreras2.ConjuntoVacio()) {
+			System.out.println(carreras2.Elegir());
+			while (!carreras1.ConjuntoVacio()) {
+				if (carreras1.Elegir() == carreras2.Elegir() || repasadas.Pertenece(carreras1.Elegir())) {
+					carreras1.Sacar(carreras1.Elegir());
+				}
+				else {
+					ConjuntoTDA materias1 = var.Recuperar(carreras1.Elegir());
+					ConjuntoTDA materias2 = var.Recuperar(carreras2.Elegir());
+					ConjuntoTDA mnc = check(materias1,materias2);
+					mostrarmaterias(carreras1, carreras2, mnc);
+					carreras1.Sacar(carreras1.Elegir());
+					
+				}
+			}
+			
+			repasadas.Agregar(carreras2.Elegir());
+			carreras2.Sacar(carreras2.Elegir());
+			carreras1 = var.Claves();
+		}
+	}	
+	
+	public static ConjuntoTDA check(ConjuntoTDA a, ConjuntoTDA b) {
+		ConjuntoTDA mnc = new ConjuntoLD();
+		mnc.InicializarConjunto();
+		while (!a.ConjuntoVacio()) {
+			if (!b.Pertenece(a.Elegir())) {
+				mnc.Agregar(a.Elegir());
+				a.Sacar(a.Elegir());
+			}
+			else
+				a.Sacar(a.Elegir());
+		}
+		if (b.ConjuntoVacio()) {
+			while(!a.ConjuntoVacio()) {
+				mnc.Agregar(a.Elegir());
+				a.Sacar(a.Elegir());
+			}
+		}
+		else
+			while(!b.ConjuntoVacio()) {
+				mnc.Agregar(b.Elegir());
+				b.Sacar(b.Elegir());
+			}
+		return mnc;
+	}
+	
+	public static void mostrarmaterias(ConjuntoTDA car1, ConjuntoTDA car2, ConjuntoTDA mnc) {
+		System.out.println("Materias de la carrera " + car1.Elegir() + " que no están en la materia " + car2.Elegir() + " son:");
+		while(!mnc.ConjuntoVacio()) {System.out.println(mnc.Elegir());mnc.Sacar(mnc.Elegir());
+		}
+	}
+	
+	public static int comparaConjuntos(ConjuntoTDA a, ConjuntoTDA b) {
+		int estaenb = 0;
+		ConjuntoTDA aux = new ConjuntoLD ();		
+		aux.InicializarConjunto();
+		copiarConjunto(a , aux);
+		int elemento =0;
+		
+		if (!aux.ConjuntoVacio() && !b.ConjuntoVacio()) {
+			while(!aux.ConjuntoVacio()) {
+			elemento =aux.Elegir();
+			if (b.Pertenece(elemento)) {
+				estaenb = estaenb+1;
+			}
+			
+			//a.Agregar(elemento);
+			aux.Sacar(elemento);
+			
+			}		
+		}
+		//a = aux;
+	return estaenb;
+	}
 
+	public static void copiarConjunto(ConjuntoTDA c1,ConjuntoTDA c2){
+		ConjuntoLD aux = new ConjuntoLD();
+		aux.InicializarConjunto();
+		pasarConjunto(c1,aux);
+		while(!aux.ConjuntoVacio()){
+			c2.Agregar(aux.Elegir());
+			c1.Agregar(aux.Elegir());
+			aux.Sacar(aux.Elegir());
+		}
+	}
+	
+    public static void pasarConjunto(ConjuntoTDA origen, ConjuntoTDA destino){
+		
+		int elemento = 0;
+		
+		while(!origen.ConjuntoVacio()){
+			
+			elemento = origen.Elegir();			
+			destino.Agregar(elemento);			
+			origen.Sacar(elemento);			
+		}		
+
+	}
+
+	
 	/*
 	 * 
 	 * 
